a954563d110ae330aa0f55fbb245d02c924c9353
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2022-08-10 20:38:14 +0200

    add subscribe spec

--- src/services/instagram/actions.py
+++ src/services/instagram/actions.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def like_post(post_url:str) -> None:
     """Likes a post with its url.
         # Returns
@@ -20,3 +23,13 @@ def share_post_in_story(post_url: str) -> None:
         - None if everything went well
     """
     pass
+
+
+def subscribe_to_user(user: str) -> None:
+    """Subscribes to a user with its uuid."""
+    pass
+
+
+def subscribe_to_multiple_users(users: List[str]) -> None:
+    for user in users:
+        subscribe_to_user(user)
