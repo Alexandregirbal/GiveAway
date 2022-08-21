0c95012a4be7a82ba1c6a1020125034f65117824
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2022-08-21 18:20:32 +0200

    /home/alexandre-girbal/Desktop/GiveAway/venv/bin/python

--- src/services/instagram/actions.py
+++ src/services/instagram/actions.py
@@ -1,11 +1,32 @@
+from time import sleep
 from typing import List
 
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.service import Service as ChromeService
+
+from src.configs.selenium_options import CHROMEDRIVER_PATH
 import src.configs.environment_variables as env
 
-def connect(username: str, password: str) -> None:
+def connect(username: str, password: str) -> Chrome:
     """Connects to Instagram with user credentials"""
-    print(username, password)
-    
+    driver = Chrome(service= ChromeService(CHROMEDRIVER_PATH))
+    with driver:
+        driver.get("https://www.instagram.com/")
+        driver.implicitly_wait(5)
+        
+        # Accept cookies
+        try:
+            driver.find_element(By.XPATH, "//button[text()='Only allow essential cookies']").click()
+        except NoSuchElementException:
+            pass
+        
+        driver.find_element(By.CSS_SELECTOR, "input[name='username']").send_keys(username)
+        driver.find_element(By.CSS_SELECTOR, "input[name='password']").send_keys(password)
+        driver.find_element(By.XPATH, "button[type='submit']").click()
+        sleep(10)
+
 
 
 def like_post(post_url:str) -> None:
