aaacdd0627afd592c2945fabacacb5487d303e52
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2022-09-29 10:38:14 +0200

    wip find giveways

--- src/services/instagram/scraping.py
+++ src/services/instagram/scraping.py
@@ -1,14 +1,33 @@
+import time
 from typing import Any, List
 
-from services.instagram.entities import PostInfo
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.service import Service as ChromeService
 
+from src.configs.selenium_options import CHROMEDRIVER_PATH
+from src.services.instagram.entities import PostInfo
 
 def find_giveaway_posts(search_filters: List[Any], limit: str = 1000) -> List[str]:
     """Finds the posts that are doing a giveaway.
         # Returns
         - A list of the posts urls
     """
-    pass
+    driver = Chrome(service= ChromeService(CHROMEDRIVER_PATH))
+    result = []
+    with driver:
+        driver.get("https://www.instagram.com/explore/tags/concours/")
+        driver.implicitly_wait(5)
+        time.sleep(10)
+        all_giveways = "article div div div div a"
+        a_elements = driver.find_elements(By.CSS_SELECTOR, all_giveways)
+        for a_element in a_elements:
+            href = a_element.get_attribute("href")
+            print(href)
+            result.append(href)
+    
+    return result
 
 
 def get_post_info(url: str) -> PostInfo:
