6abe6afb113ddc98867c9b5d31096986a463c1b8
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2022-10-23 19:14:22 +0200

    add friends

--- src/constants/get.py
+++ src/constants/get.py
@@ -4,13 +4,23 @@ from random import shuffle
 from typing import List
 from src.configs.environment_variables import PROJECT_ROOT
 
-def get_comments() -> List[dict]:
+def _load_json_file(file_path: str) -> dict:
+    """Loads a json file and returns its content"""
+    with open(file_path) as json_file:
+        json_content = json_file.read()
+
+    return json.loads(json_content)
+
+
+def _get_friends_from_json() -> List[dict]:
+    friends_file_path = os.path.join(PROJECT_ROOT, 'src', "constants", "friends.json")
+    friends = _load_json_file(friends_file_path)
+    return friends
+
+
+def _get_comments_from_json() -> List[dict]:
     comments_file_path = os.path.join(PROJECT_ROOT, 'src', "constants", "comments.json")
-
-    with open(comments_file_path) as comments_file:
-        comments_json = comments_file.read()
-
-    comments = json.loads(comments_json)
+    comments = _load_json_file(comments_file_path)
     return comments
 
 
@@ -18,13 +28,22 @@ def _get_comment_content(comment: dict) -> str:
     return comment.get("content")
 
 
-def get_random_comment(length: int = -1, level: int = 0) -> str:
-    comments = get_comments()
+def get_random_comments(length: int = 1, level: int = 0) -> str:
+    """Gets a random comment from the comments.json file.
+        Returns 1 comment by default.
+    """
+    comments = _get_comments_from_json()
         
     filtered_comments = list(filter(lambda comment: comment.get("level") == level, comments))
     shuffle(filtered_comments)
     
-    if length == -1:
-        length = len(filtered_comments)
-    
     return list(map(_get_comment_content, filtered_comments))[:length]
+
+
+def get_random_friends(length: int = 2) -> str:
+    """Gets a random friend from the friends.json file.
+        Returns 2 friends by default.
+    """
+    friends = _get_friends_from_json()
+    shuffle(friends)
+    return friends[:length]
