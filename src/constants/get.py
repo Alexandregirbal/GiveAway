fc1e0698004ddd7780609ffaa15ccd173e7405b0
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2022-10-23 18:52:07 +0200

    update comments

--- src/constants/get.py
+++ src/constants/get.py
@@ -2,7 +2,7 @@ import json
 import os
 from random import shuffle
 from typing import List
-from configs.environment_variables import PROJECT_ROOT
+from src.configs.environment_variables import PROJECT_ROOT
 
 def get_comments() -> List[dict]:
     comments_file_path = os.path.join(PROJECT_ROOT, 'src', "constants", "comments.json")
@@ -13,12 +13,18 @@ def get_comments() -> List[dict]:
     comments = json.loads(comments_json)
     return comments
 
-def get_random_comment(length: int = 0) -> str:
+
+def _get_comment_content(comment: dict) -> str:
+    return comment.get("content")
+
+
+def get_random_comment(length: int = -1, level: int = 0) -> str:
     comments = get_comments()
-    shuffle(comments)
+        
+    filtered_comments = list(filter(lambda comment: comment.get("level") == level, comments))
+    shuffle(filtered_comments)
     
-    if length == 0:
-        return comments
+    if length == -1:
+        length = len(filtered_comments)
     
-    return comments[:length]
-    
+    return list(map(_get_comment_content, filtered_comments))[:length]
